78aa822c7c79ed7b81fe8760dbf87b05afbf45be
Marcel Petrick <mail@marcelpetrick.it>
2020-03-12 16:13:52 +0100

    cppGem: auto-splitting of tuples (Python horayy)
    Change-Id: I1459b66b34096375f4bd303264aaaf568a5ef9e3

--- cppGems.cpp
+++ cppGems.cpp
@@ -1,8 +1,24 @@
 //! Sum all values in the container.
 auto const progressMaximum = std::accumulate(input.begin(), input.end(), 0);
 
+// ---------------------------------------------------------------------------
+
 //! Rename the key of some entry in a std::map. (C++17)
 std::map<std::string, int> entries;
 auto node = entries.extract(oldName);
 node.key() = newName; // explodes here, because std::string does not like to be touched
 entries.insert(std::move(node));
+
+// ---------------------------------------------------------------------------
+
+//! Return several return-values as tuple and split this automatically
+// func:
+std::tuple<float, float, float>
+Device::getProperties() const
+{
+    return std::make_tuple(1.0f, 2.0f, 3.0f);
+}
+// the call:
+auto [time, length, freq] = getProperties(); // (✿◠‿◠)
+
+// ---------------------------------------------------------------------------
